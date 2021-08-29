b25a0a3a2ff4227f0be2126db9827c7843f6c3c5
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-08-29 17:13:36 +0300

    Update templ.js

--- partials/templ.js
+++ partials/templ.js
@@ -1,2 +1,6 @@
 import constructor from '../option/menu.json';
-import menu from '../option/'
+import menu from '../option/options.hbs;'
+
+const menuList = document.querySelector('.js-menu')
+const markup = constructor(menu)
+menuList.insertAdjacentHTML('afterbegin', markup)
