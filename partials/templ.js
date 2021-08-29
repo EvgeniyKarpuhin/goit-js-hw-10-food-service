792963d0986fd55c92ce60dedb6881971b5a82d2
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-08-29 18:01:53 +0300

    Update templ.js

--- partials/templ.js
+++ partials/templ.js
@@ -1,5 +1,5 @@
 import constructor from '../option/menu.json';
-import menu from '../option/options.hbs;'
+import menu from '../option/options.hbs';
 
 const menuList = document.querySelector('.js-menu')
 const markup = constructor(menu)
