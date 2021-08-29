cfc192d7fbb3021cc73228768efbd986d1783a1c
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-08-29 16:48:39 +0300

    Update theme.js

--- partials/theme.js
+++ partials/theme.js
@@ -7,7 +7,7 @@ const Theme = {
   const saveTheme = localStorage.getItem('Theme')
   const switchTheme = document.querySelector('.theme-switch__toggle')
 
-  switchTheme.addEventListener('change', onSwitchCheckBox)
+  switchTheme.addEventListener('change', checkSwitch)
   bodyTheme.classList.add('light-theme')
 
   const themeInSave = function() {
@@ -16,3 +16,20 @@ const Theme = {
         switchTheme.setAttribute('cheked', true)
       }
   }
+
+  themeInSave()
+
+function dakrTheme() {
+    bodyTheme.classList.add('dark-theme') || bodyTheme.classList.remove('light-theme')
+    localStorage.setItem('Theme', 'dark');
+}
+
+function lightTheme() {
+    bodyTheme.classList.add('light-theme') || bodyTheme.classList.remove('dark-theme')
+    localStorage.setItem('Theme', 'light');
+}
+
+function checkSwitch(e) {
+    dakrTheme()
+    if(!e.target.checked) {lightTheme()}
+}
