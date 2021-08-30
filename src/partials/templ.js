6e1be2b39fab647b21326ea640e080859be73ed1
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-08-30 13:47:48 +0300

    Create templ.js

---
+++ src/partials/templ.js
@@ -0,0 +1,7 @@
+import constructor from '../option/menu.hbs';
+import menu from '../../menu.json';
+
+
+const menuList = document.querySelector('.js-menu');
+const markup = constructor(menu);
+menuList.insertAdjacentHTML('afterbegin', markup);
